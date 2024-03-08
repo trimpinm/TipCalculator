f35a52141820e3866142a87cb6cbb5eeda3776aa
Maggie Trimpin <trimpinm@gmail.com>
2024-03-08 12:05:25 -0500

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Maggie Trimpin on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
